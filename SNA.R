93907d91cb3ff0b87a8658e6372c7d2606605927
Liubo <liubomyrb@gmail.com>
2018-07-25 08:29:20 +0200

    Add files via upload

--- SNA.R
+++ SNA.R
@@ -2,8 +2,13 @@
 #Liubomyr Bregman
 #Lviv DS summer school 
 
-#################################### Illustration of Centralities 
+####################################
+#           Centralities          #
+####################################
+
 library(igraph)
+library(readr)
+library(ggplot2)
 
 g <- graph.formula(  Andriy----Katia:Dima:Fedir:Kuzma,
                      Katia--Andriy:Dima:Georgiy:Ed,
@@ -43,27 +48,56 @@ V(g)$size <- (eigen_centrality$vector)/mean(eigen_centrality$vector)*10
 plot(g)
 
 
+####################################
+#             COMMUNITY            #
+####################################
+
+#edge.betweenness
+st <- Sys.time()
+com1 <- edge.betweenness.community(g)
+Sys.time() - st 
+
+plot(com1, g)
+
+#Fastgreed
+st <- Sys.time()
+com2 <- fastgreedy.community(g)
+Sys.time() - st 
+
+plot(com2, g)
+
+#Walktrap or short random walks
+
+st <- Sys.time()
+com3 <- walktrap.community(g,  steps = 4)
+Sys.time() - st 
+
+plot(com3, g)
 
 
-##################################### CASE STUDY TELCO 
+
+
+
+####################################
+#             CASE TELCO           #
+####################################
 
 
 calls <-read_csv("SNA_test.csv")
-install.packages('igraph')
+summary(calls)
 
-library(igraph)
 ##Creating a graph
 graph1<- igraph::graph_from_data_frame(calls, directed = T)
-V(graph1)$frame.color <- "white"
-V(graph1)$label <- "" 
-V(graph1)$size <- igraph::degree(graph1)*calls$nocalls/6
-V(graph1)$color <- igraph::degree(graph1)
-E(graph1)$width <- calls$nocalls/12
-E(graph1)$length <- (200 - as.numeric(calls$nocalls))/200
-E(graph1)$arrow.mode <- 0
+  #Vertex
+  V(graph1)$frame.color <- "white"
+  V(graph1)$label <- "" 
+  V(graph1)$size <- igraph::degree(graph1)*calls$nocalls/6
+  V(graph1)$color <- igraph::degree(graph1)
+  #Edges
+  E(graph1)$width <- calls$nocalls/12
+  E(graph1)$length <- (200 - as.numeric(calls$nocalls))/200
+  E(graph1)$arrow.mode <- 0
 
-
-#Calculation of centralities
 
 
 #Degree (number of ties)
@@ -74,32 +108,11 @@ degree3 <- igraph::degree(graph1, mode = "out")
 degree_ig_time <- Sys.time() - st_0
 
 
-
 library(ggplot2)
-
 qplot(degree(graph1), bins = 150)
-
 deg_dist <- degree_distribution(graph1, cumulative=T, mode="all")
 plot( x=0:max(degree(graph1)), y=1-deg_dist, pch=19, cex=1.2, col="orange",
       xlab="Degree", ylab="Cumulative Frequency")
-
-
-
-
-qplot(degree(graph1, mode = 'in'), bins = 150)
-deg_dist2 <- degree_distribution(graph1, cumulative=T, mode="out")
-plot( x=0:max(degree(graph1, mode = "in")), y=1-deg_dist_2, pch=19, cex=1, col="orange",
-      xlab="InDegree", ylab="Cumulative Frequency")
-
-
-
-
-
-qplot(degree(graph1, mode = 'out'), bins = 150)
-deg_dist3 <- degree_distribution(graph1, cumulative=T, mode="out")
-plot( x=0:max(degree(graph1, mode = "out")), y=1-deg_dist3 , pch=19, cex=1, col="orange",
-      xlab="outDegree", ylab="Cumulative Frequency")
-
 
 
 #Closeness (centrality based on distance to others in the graph)
@@ -111,19 +124,57 @@ closeness_ig_time <- Sys.time() - st_2
 closeness(graph1, vids = V(graph1), mode = c("out", "in", "all", "total"),
           weights = NULL, normalized = FALSE)
 
-estimate_closeness(graph1, vids = V(graph1), mode = c("out", "in", "all",
-                                                    "total"), cutoff, weights = NULL, normalized = FALSE)
 
-
-
+closeness2 <- estimate_closeness(graph1,
+                   vids = V(graph1), 
+                   cutoff = 2,
+                   weights = NULL, 
+                   normalized = FALSE)
 
 #Eigenvector (centrality proportional to the sum of connection centralities)
 st_3 <- Sys.time()
-eigenvector1 <-igraph::evcent(graph1)
+  eigenvector1 <-igraph::evcent(graph1)
 eigen_ig_time <- Sys.time() - st_3
-
 
 #Betweenness
 st_4 <- Sys.time()
-betweenness1 <- igraph::betweenness(graph1)
+  betweenness1 <- igraph::betweenness(graph1)
 between_ig_time <-  Sys.time() - st_4
+
+st_5 <- Sys.time()
+  betweenness2 <- igraph::estimate_betweenness(graph1, cutoff = 2)
+between_ig_time <-  Sys.time() - st_4
+
+
+
+
+####################################
+#             COMMUNITY TELCO      #
+####################################
+
+
+#edge.betweenness
+
+st <- Sys.time()
+com1 <- edge.betweenness.community(graph1)
+Sys.time() - st 
+
+plot(com1, graph1)
+
+modularity(com1)
+#Fastgreed
+st <- Sys.time()
+com2 <- fastgreedy.community(graph1)
+Sys.time() - st 
+
+plot(com2, graph1)
+
+#Walktrap or short random walks
+
+st <- Sys.time()
+com3 <- walktrap.community(graph1,  steps = 4)
+Sys.time() - st 
+
+plot(com3, g)
+
+
